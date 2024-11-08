4817e00cd3d01a7392040e6abe268eaf5a79eefb
qwyx <mc010116@gmail.com>
2024-11-08 16:01:11 +0100

    when click duplicate circle whith random whith and color

--- main.js
+++ main.js
@@ -1,32 +1,56 @@
-const circle = document.getElementById('circle');
+const circle = document.getElementById("circle");
 
 let isMoving = false;
+let lastMouseX = 0;
+let lastMouseY = 0;
 
-const startMoving = (event) => {
-    isMoving = true;
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-
-    circle.style.left = `${mouseX - circle.offsetWidth / 2}px`;
-    circle.style.top = `${mouseY - circle.offsetHeight / 2}px`;
-
-    document.addEventListener('mousemove', moveCircle);
+const getRandomColor = () => {
+	const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+	return `#${randomColor}`;
 };
 
-const stopMoving = () => {
-    isMoving = false;
-    document.removeEventListener('mousemove', moveCircle);
+const getRandomSize = () => {
+	const size = Math.floor(Math.random() * 81) + 20;
+	return `${size}px`;
+};
+
+const startMoving = (event) => {
+	isMoving = true;
+	lastMouseX = event.clientX;
+	lastMouseY = event.clientY;
+
+	circle.style.left = `${lastMouseX - circle.offsetWidth / 2}px`;
+	circle.style.top = `${lastMouseY - circle.offsetHeight / 2}px`;
+    circle.style.backgroundColor = getRandomColor()
+
+	const newdiv = document.createElement("div");
+	newdiv.className = "circle";
+	newdiv.style.left = `${event.clientX - 25}px`;
+	newdiv.style.top = `${event.clientY - 25}px`;
+	newdiv.style.backgroundColor = getRandomColor();
+	newdiv.style.width = getRandomSize();
+	newdiv.style.height = newdiv.style.width;
+
+	document.body.appendChild(newdiv);
+
+	document.addEventListener("mousemove", moveCircle);
+};
+
+const stopMoving = (event) => {
+	isMoving = false;
+
+	document.removeEventListener("mousemove", moveCircle);
 };
 
 const moveCircle = (event) => {
-    if (!isMoving) return;
-    
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
+	if (!isMoving) return;
 
-    circle.style.left = `${mouseX - circle.offsetWidth / 2}px`;
-    circle.style.top = `${mouseY - circle.offsetHeight / 2}px`;
+	lastMouseX = event.clientX;
+	lastMouseY = event.clientY;
+
+	circle.style.left = `${lastMouseX - circle.offsetWidth / 2}px`;
+	circle.style.top = `${lastMouseY - circle.offsetHeight / 2}px`;
 };
 
-document.addEventListener('mousedown', startMoving);
-document.addEventListener('mouseup', stopMoving);
+document.addEventListener("mousedown", startMoving);
+document.addEventListener("mouseup", stopMoving);
